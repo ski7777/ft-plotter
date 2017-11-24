56f8e787ffb0fbddda5a22c56b440e80a4ee4d8b
Raphael Jacob <r.jacob2002@gmail.com>
2017-11-25 00:31:16 +0100

    Add basic test program

---
+++ src/main.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+from ftrobopy import ftrobopy
+from axis import *
+import time
+
+
+# initialize controllers
+txt = ftrobopy()
+
+# prepare X-Axis
+XAxis = Axis(txt, 1, [1, 2])
+XAxis.maxPos = 7700
+# initialize X-Axis
+XAxis.initialize()
+
+# move a bit away from the init position to "present" the X-Axis
+XAxis.goPos(250)
+while not XAxis.posReached():
+    pass
+time.sleep(1)
+# move back to 0
+XAxis.goPos(0)
+while not XAxis.posReached():
+    pass
