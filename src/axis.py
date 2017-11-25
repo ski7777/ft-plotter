c05430f1ff332a2c4384e262dbcf0429985d8f10
Raphael Jacob <r.jacob2002@gmail.com>
2017-11-25 21:11:39 +0100

    Axis: Add more documentation Set watchdog reanimation values via variable

--- src/axis.py
+++ src/axis.py
@@ -14,20 +14,22 @@ class Axis:
         assert(mot in range(1, 5))
         assert(type(end) == list and len(end) >= 1)
         # initialize the main properties
-        self.outer = outer
-        self.mot = self.outer.motor(mot)
-        self.mot.setSpeed(0)
-        self.mot.setDistance(0)
-        self.speed = 512
-        self.initSpeed = 512
-        self.direction = 0
-        self.distance = 0
-        self.initialized = False
-        self.pos = 0
-        self.maxPos = -1
-        self.wdInterval = 0.1
-        self.wdStrikeTime = 2
-        self.wdMinMoveAlertReset = 20
+        self.outer = outer  # contains the ftrobopy(-like) object
+        self.mot = self.outer.motor(mot)  # the motor object
+        self.speed = 512  # normal speed
+        self.initSpeed = 512  # initialization speed
+        self.direction = 0  # current direction
+        self.distance = 0  # distance of current movement
+        self.initialized = False  # initialization status
+        self.pos = 0  # current position
+        self.maxPos = -1  # maximum position, -1 = unset
+        self.wdInterval = 0.1  # watchdogcheck interval
+        self.wdStrikeTime = 2.0  # watchdog trigger time
+        self.wdMinMoveAlertReset = 20  # minimum distance (in wdMotOnTime) to reset the watchdog
+        self.wdMotOffTime = 1.0  # watchdog reanimation: motor off time
+        self.wdMotOnTime = 4.0  # watchdog reanimation: motor on time
+        # reset the motor
+        self.mot.stop()
         # initialize all end stops
         self.end = []
         for e in set(end):
@@ -35,7 +37,7 @@ class Axis:
             self.end.append(self.outer.input(e))
 
     def checkAllEndPressed(self):
-        # return true if all end stopsare pressed, otherwise false
+        # return true if all end stops are pressed, otherwise false
         for e in self.end:
             if e.state() == 0:
                 return(False)
@@ -47,7 +49,7 @@ class Axis:
             self.mot.setSpeed(-self.initSpeed)
             while not self.checkAllEndPressed():  # check whether init position is reached
                 time.sleep(0.1)
-            self.mot.setSpeed(0)
+            self.mot.setSpeed(0)  # use setSpeed, not stop! It would reset the finished state! --> AssertError
         self.initialized = True
         self.pos = 0
 
@@ -60,10 +62,10 @@ class Axis:
         # check whether target position is in range of axis (if set)
         if self.maxPos != -1:
             assert(pos in range(self.maxPos + 1))
+        assert(self.initialized)  # check whether the axis is initialized
         assert(self.posReached())  # move only if old target position is reached
-        # prepare by setting speed and distance to 0
-        self.mot.setSpeed(0)
-        self.mot.setDistance(0)
+        # reset the motor
+        self.mot.stop()
         # calculate speed and distance depended on the target position relative to current position
         if pos < self.pos:
             self.distance = self.pos - pos
@@ -110,10 +112,10 @@ class Axis:
                     # reanimate the motor until it is runnig ;-)
                     while lastPos + self.wdMinMoveAlertReset > self.mot.getCurrentDistance():
                         # stop the motor for a moment and restart it
-                        self.mot.setSpeed(0)
-                        time.sleep(1)
+                        self.mot.setSpeed(0)  # use setSpeed, not stop! It would reset the distance!
+                        time.sleep(self.wdMotOffTime)
                         self.mot.setSpeed(self.direction * self.speed)
-                        time.sleep(4)
+                        time.sleep(self.wdMotOnTime)
                     motStop = 0  # motor is runnig :) Save this!
                 lastPos = self.mot.getCurrentDistance()  # save the last moving position
             else:
